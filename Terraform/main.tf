3b58b83833eb9a93133fa6efaddd873fa586e071
azizul-orvee <orvee16@gmail.com>
2023-08-02 03:25:27 +0600

    tf s3 CF run

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,72 +1,35 @@
+# terraform {
+#   required_version = ">= 0.13"
+
+#   backend "s3" {
+#     bucket = "your-s3-bucket-name"
+#     key    = "terraform.tfstate"
+#     region = "us-west-2"
+#   }
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_cognito_user_pool" "main" {
-  name = "my_user_pool"
-
-  auto_verified_attributes = ["email"]
-  email_verification_message = "Scopic Software DevOps task verification code is {####}."
-
-    schema {
-    attribute_data_type      = "String"
-    developer_only_attribute = false
-    mutable                  = true
-    name                     = "email"
-    required                 = true
-
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-  
-  password_policy {
-    minimum_length    = 8
-    require_lowercase = false
-    require_numbers   = true
-    require_symbols   = false
-    require_uppercase = false
-  }
-
-  username_configuration {
-    case_sensitive = false
-  }
+module "auth" {
+  source = "./modules/auth"
+  # If your module accepts variables, they can be passed in here.
+  # variable1 = "value1"
+  # variable2 = "value2"
 }
 
-resource "aws_cognito_user_pool_client" "client" {
-  name = "my_user_pool_client"
-
-  user_pool_id = aws_cognito_user_pool.main.id
-
-  explicit_auth_flows = [
-    "ALLOW_USER_SRP_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH",
-  ]
+module "frontend" {
+  source = "./modules/frontend"
+  # variable1 = "value1"
 }
 
-resource "aws_cognito_user_pool_domain" "main" {
-  domain       = "scopic-devops889966"
-  user_pool_id = aws_cognito_user_pool.main.id
+module "backend" {
+  source = "./modules/backend"
+  # variable1 = "value1"
 }
 
-
-output "USER_POOL_APP_CLIENT_ID" {
-  description = "The ID of the App Client"
-  value       = aws_cognito_user_pool_client.client.id
-  sensitive   = true
+module "database" {
+  source = "./modules/database"
+  # variable1 = "value1"
 }
-
-output "USER_POOL_ID" {
-  description = "The ID of the User Pool"
-  value       = aws_cognito_user_pool.main.id
-  sensitive   = true
-}
-
-output "REGION" {
-  description = "AWS Region"
-  value       = "us-east-1"
-  sensitive   = true
-}
-
-# terraform output -json > output.json
---
+++ Terraform/outputs.tf
@@ -0,0 +1,50 @@
+// Outputs from auth module
+output "USER_POOL_APP_CLIENT_ID" {
+  description = "The ID of the App Client"
+  value       = module.auth.USER_POOL_APP_CLIENT_ID
+  sensitive   = true
+}
+
+output "USER_POOL_ID" {
+  description = "The ID of the User Pool"
+  value       = module.auth.USER_POOL_ID
+  sensitive   = true
+}
+
+output "REGION" {
+  description = "AWS Region"
+  value       = module.auth.REGION
+  sensitive   = true
+}
+
+// Outputs from frontend module
+output "frontend_bucket_name" {
+  description = "The name of the frontend bucket"
+  value       = module.frontend.bucket_name
+}
+
+output "frontend_cloudfront_distribution_id" {
+  description = "The ID of the frontend CloudFront distribution"
+  value       = module.frontend.cloudfront_distribution_id
+}
+
+output "frontend_cloudfront_distribution_domain_name" {
+  description = "The domain name of the frontend CloudFront distribution"
+  value       = module.frontend.cloudfront_distribution_domain_name
+}
+
+
+
+// Outputs from backend module
+// output "BACKEND_OUTPUT" {
+//   description = "Description here"
+//   value       = module.backend.BACKEND_OUTPUT
+//   sensitive   = false
+// }
+
+// Outputs from database module
+// output "DATABASE_OUTPUT" {
+//   description = "Description here"
+//   value       = module.database.DATABASE_OUTPUT
+//   sensitive   = false
+// }
